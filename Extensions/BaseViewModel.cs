9d91ad808b3f9f729a71543008e9cb5c24215d96
UltimateSeeSharp <91130013+UltimateSeeSharp@users.noreply.github.com>
2022-01-25 13:51:11 +0100

    Create BaseViewModel.cs

---
+++ Extensions/BaseViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVM.Template.Extensions;
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string propertyname = null!)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+    }
+}
